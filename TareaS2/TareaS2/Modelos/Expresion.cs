8289982f7218e8bc1e5b8458264075d195b6915b
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-25 20:57:17 -0600

    clase expresion y e instancias creadas

---
+++ TareaS2/TareaS2/Modelos/Expresion.cs
@@ -0,0 +1,48 @@
+﻿namespace TareaS2.Modelos
+{
+    public class Expresion { 
+        
+        public int numero1 { get; set; } = 0;
+        public int numero2 { get; set; } = 0;
+
+        public int suma()
+        {
+            return numero1 + numero2;
+        }
+        public int resta()
+        {
+            return numero1 - numero2;
+        }
+        public int multiplicacion()
+        {
+            return numero1 * numero2;
+        }
+        public int division()
+        {
+            return numero1 / numero2;
+        }
+
+        public int maximo()
+        {
+            if(numero1 > numero2)
+            {
+                return numero1;
+            }
+            else
+            {
+                return numero2;
+            }
+        }
+        public int minimo()
+        {
+            if (numero1 < numero2)
+            {
+                return numero1;
+            }
+            else
+            {
+                return numero2;
+            }
+        }
+    }
+}
---
+++ TareaS2/TareaS2/Program.cs
@@ -0,0 +1,31 @@
+﻿using TareaS2.Modelos;
+
+Expresion c1 = new Expresion()
+{
+    numero1 = 10,
+    numero2 = 20,
+};
+
+Console.WriteLine("========== INICIO =========== ");
+
+Expresion c2 = new Expresion()
+{
+    numero1 = 30,
+    numero2 = 40,
+};
+
+List<Expresion> listEx = new List<Expresion>();
+
+listEx.Add(c1);
+listEx.Add(c2);
+
+foreach (Expresion e in listEx)
+{
+    Console.WriteLine($"Suma: {e.suma()}");
+    Console.WriteLine($"Resta: {e.resta()}");
+    Console.WriteLine($"Multiplicación: {e.multiplicacion()}");
+    Console.WriteLine($"División: {e.division()}");
+    Console.WriteLine($"Maximo: {e.maximo()}");
+    Console.WriteLine($"Minimo: {e.minimo()}");
+    Console.WriteLine("=======");
+}
